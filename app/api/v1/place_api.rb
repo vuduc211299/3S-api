46a275b6b8c21844ab1b47bc69d6a846e49481c2
Ducvu99 <vuduc112299@gmail.com>
2020-12-11 10:29:46 +0700

    fix place api + seed

--- app/api/v1/place_api.rb
+++ app/api/v1/place_api.rb
@@ -62,7 +62,27 @@ class PlaceApi < ApiV1
     get "/:place_id" do
       place = Place.find_by id: params[:place_id]
 
-      return render_success_response(:ok, PlaceResFormat, {data: place}, message: "Get place successfully") if place
+      result = {}
+      result[:name] = place.name
+      result[:id] = place.id
+      result[:details] = place.details
+      result[:address] = place.address
+      result[:place_type] = place.place_type
+      result[:host] = User.find_by id: place.user_id
+      result[:schedule_price] = place.schedule_price
+      result[:room] = place.room
+      result[:policy] = place.policy
+      result[:rule] = place.rule
+      result[:ratings] = place.ratings
+      facility = []
+      PlaceFacility.where(place_id: place.id).each do |f|
+        facility << Facility.find_by(id: f.facility_id).name
+      end
+
+      result[:place_facilities] = facility
+      result[:overviews] = place.overviews
+
+      return render_success_response(:ok, PlaceResFormat, {data: result}, message: "Get place successfully") if place
 
       error!("Place not found", 404)
     end
@@ -74,9 +94,35 @@ class PlaceApi < ApiV1
     end
 
     get "/city/:city" do
+      results = []
+
       places = Place.where city: params[:city]
 
-      return render_success_response(:ok, PlaceResFormat, {data: places}, message: "Get place successfully") if places
+      places.each do |place|
+        result = {}
+        result[:name] = place.name
+        result[:id] = place.id
+        result[:details] = place.details
+        result[:host] = User.find_by id: place.user_id
+        result[:address] = place.address
+        result[:place_type] = place.place_type
+        result[:schedule_price] = place.schedule_price
+        result[:room] = place.room
+        result[:policy] = place.policy
+        result[:rule] = place.rule
+        result[:ratings] = place.ratings
+        facility = []
+        PlaceFacility.where(place_id: place.id).each do |f|
+          facility << Facility.find_by(id: f.facility_id).name
+        end
+
+        result[:place_facilities] = facility
+        result[:overviews] = place.overviews
+
+        results << result
+      end
+
+      return render_success_response(:ok, PlaceResFormat, {data: results}, message: "Get place successfully") if places
 
       error!(places.errors.full_messages[0], :bad_request)
     end
